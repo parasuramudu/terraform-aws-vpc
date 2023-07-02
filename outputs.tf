fe299cbb68f0c2838dfe2a2d059c540540aca351
koppadaram <koppadaparasuramudu@gmail.com>
2023-07-02 10:58:03 +0530

    added out put values

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,18 @@
 output "azs_labels" {
   value = local.azs_labels
 }
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+  
+}
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id 
+
+}
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id 
+}
